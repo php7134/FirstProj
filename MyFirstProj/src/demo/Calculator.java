273dd9c7b7d71bffb803939f673e43013224c04a
Win <Win@Win-PC>
2018-06-03 00:17:30 +0530

    First commit
    For all file upload

---
+++ MyFirstProj/src/demo/Calculator.java
@@ -0,0 +1,22 @@
+package demo;
+
+public class Calculator {
+	
+
+	public static void main(String[] args)
+	{
+		int num1=20;
+		int num2=10;
+		int res;
+	
+		
+		res=num1+num2;
+		System.out.println("Addition="+res);
+		
+		res=num1-num2;
+		System.out.println("Substraction="+res);
+		
+		}
+		
+
+	}
